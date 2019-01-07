c6a358f38cc1049391ae76cf7c97782fef50a036
Praveen Lingala <praveenlingala92@gmail.com>
2019-01-07 21:10:57 +0530

    env path change

--- src/configurations/environment.js
+++ src/configurations/environment.js
@@ -1,6 +1,6 @@
 export const Environment = {
     MW_END_POINT_URL_1 : 'https://www.kiposcollective.com:8065/',
     MW_END_POINT_URL : 'https://desolate-tundra-77543.herokuapp.com/',
-    STATIC_FILES_END_POINT_URL: 'http://13.233.199.186:3000/'
+    STATIC_FILES_END_POINT_URL: 'http://localhost:4200/'
    // MW_END_POINT_URL    : 'http://2bedbcba.ngrok.io/'
 }
