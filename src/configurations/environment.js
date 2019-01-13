59c63e1bdaebcb831a5b218c54d850b591eca27e
Rama <rama@github.com>
2019-01-14 00:36:58 +0530

    heroku runtime

--- src/configurations/environment.js
+++ src/configurations/environment.js
@@ -1,5 +1,9 @@
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+const env = runtimeEnv();
+
 export const Environment = {
-    MW_END_POINT_URL : process.env.REACT_APP_API_ENDPOINT,
-    STATIC_FILES_END_POINT_URL: process.env.REACT_APP_STATIC_FILE_URL,
-    GOOGLE_MAP_KEY: process.env.REACT_APP_GOOGLE_MAP_KEY 
+    MW_END_POINT_URL : env.REACT_APP_API_ENDPOINT,
+    STATIC_FILES_END_POINT_URL: nv.REACT_APP_STATIC_FILE_URL,
+    GOOGLE_MAP_KEY: env.REACT_APP_GOOGLE_MAP_KEY 
 }
